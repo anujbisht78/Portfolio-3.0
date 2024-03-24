b4ca63de4e768ddc66f653a5491ea80c6ac8f394
anujbisht78 <anujbisht3540@gmail.com>
2024-03-25 00:07:34 +0530

    resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/anujbisht78",
   resume:
-    "https://drive.google.com/file/d/1tSlZwJAUzl3brj6jxY4CSRE6i5ExAH18/view?usp=sharing",
+    "https://drive.google.com/file/d/17kB1hhdQhrR5UCkBP-JUX6HhjHTk12yt/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/anujbisht78/",
   twitter: "https://twitter.com/AnujBisht78",
   insta: "https://www.instagram.com/anujbisht78/?hl=en",
